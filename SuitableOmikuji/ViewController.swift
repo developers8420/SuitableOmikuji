d1f814be4fe603b2a36921e1f8ae99edf68c4be2
秋山友也 <akitomo0114@gmail.com>
2015-09-23 16:51:55 +0900

    Initial Commit

---
+++ SuitableOmikuji/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuitableOmikuji
+//
+//  Created by 秋山友也 on 2015/09/23.
+//  Copyright (c) 2015年 developers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
